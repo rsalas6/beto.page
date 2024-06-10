eb886af0e313119a4ec4cd9a9baf14788e447345
Roberto Salas <hi@rsalas.dev>
2024-06-10 15:43:43 -0600

    fix: og images

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-micro.vercel.app",
+  site: "https://beto.page",
   integrations: [tailwind(), sitemap(), mdx(), pagefind(), vue()],
   markdown: {
     shikiConfig: {
